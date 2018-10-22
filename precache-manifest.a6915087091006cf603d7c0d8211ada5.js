bb9f36584a86779e63656ffb459ad6d5fa051ae4
gouravjeet <gouravjeet.com>
2018-10-22 08:40:24 -0400

    Updates

---
+++ precache-manifest.a6915087091006cf603d7c0d8211ada5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "bcfab2eecc79b0376183",
+    "url": "/tictactoe/static/js/main.bcfab2ee.chunk.js"
+  },
+  {
+    "revision": "36d5fb947b0e365c11c2",
+    "url": "/tictactoe/static/js/1.36d5fb94.chunk.js"
+  },
+  {
+    "revision": "bcfab2eecc79b0376183",
+    "url": "/tictactoe/static/css/main.8fac9ba8.chunk.css"
+  },
+  {
+    "revision": "007c84c529caa34a132bd26ac26cdf52",
+    "url": "/tictactoe/index.html"
+  }
+];
